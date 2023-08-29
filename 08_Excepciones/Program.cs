98b7979ac4180d1b83626f833e1a34c5d3c7aa72
José Luis Chaudary Sucre <jose.chaudary@grupomost.com>
2023-08-29 11:01:03 -0300

    Cambios

---
+++ 08_Excepciones/Program.cs
@@ -0,0 +1,40 @@
+﻿
+try
+{
+    try
+    {
+        AbrirArchivo("C:\\Users\\chaud\\Desktop\\no_existe.txt");
+        //throw new Exception("Aqui lanzo una excepción mía");
+    }
+    catch (Exception ex)
+    {
+        throw new Exception("Ocurrió un error al ejecutar el método AbrirArchivo, el error fue: " + ex.Message);
+        //Console.WriteLine("Ocurrió un error: " + ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Finally ejecutado!");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocurrió un error dentro del try-cath. El error fue: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Finally de mas afuera ejecutado!");
+}
+
+Console.WriteLine("El programa ha finalizado!");
+
+void AbrirArchivo(string path)
+{
+	try
+	{
+        File.Open(path, FileMode.Open);
+    }
+	catch (Exception)
+	{
+		throw new Exception("Lo siento, este archivo no existe en la ruta especificada.");
+	}
+}
---
+++ 09_Manipulacion_De_Archivos/Program.cs
@@ -0,0 +1,25 @@
+﻿List<string> productos = new()
+{
+    "Cachito",
+    "Tequeños",
+    "Queso",
+};
+
+
+var path = "C:\\Users\\chaud\\Desktop\\practica.naho";
+var contenido = File.ReadAllText(path);
+var productosSeparados = contenido.Split("\n");
+
+foreach (var producto in productosSeparados)
+{
+    Console.WriteLine(producto);
+}
+
+//Console.WriteLine(contenido);
+
+//File.AppendAllLines(path, productos);
+
+//Console.WriteLine("------------------------------------------------------------------------------");
+
+//var contenidoFinal = File.ReadAllText(path);
+//Console.WriteLine(contenidoFinal);
